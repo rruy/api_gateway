b8bd7f389a140818e0b6d14257ecc07402ce39f8
Ricardo Ruy <ricardo.rruy@hotmail.com>
2022-06-19 00:18:06 -0300

    Added of unit tests for rate limits

---
+++ spec/rate_limit/frequency/rpm_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Frequency/rpm rule request' do
+  include Rack::Test::Methods
+
+  it 'should be allowed if not exceed 1 request per min' do
+    get '/freq/rpm', {}, { 'HTTP_ACCEPT' => 'text/html' }
+    expect(last_response.body).to show_allowed_response
+  end
+
+  it 'should not be allowed if exceed 1 request per min' do
+    2.times { get '/freq/rpm', {}, { 'HTTP_ACCEPT' => 'text/html' } }
+    expect(last_response.body).to show_not_allowed_response
+  end
+end
---
+++ spec/rate_limit/json_request_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'json request' do
+  include Rack::Test::Methods
+  it 'should receive allowed' do
+    2.times { get '/json', {}, { 'HTTP_ACCEPT' => 'application/json' } }
+    expect(last_response.content_type).to eq 'application/json'
+  end
+end
